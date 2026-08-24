7d6b84e4b16f1039f939cc09d92f992fae74cad2
Kritman Kumar Rao <kritman.rao@joshtechnologygroup.com>
2026-08-24 22:46:34 +0530

    [KR-A3-03]: Added PaginationResponse

---
+++ RestaurantServer/Controllers/RestaurantController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantServer.Controllers
+{
+    [RoutePrefix("api/restaurants")]
+    public class RestaurantController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<IHttpActionResult> GetAllRestaurants(CancellationToken cancellationToken)
+        {
+            return Ok("running");
+        }
+    }
+}
---
+++ RestaurantServer/DTOs/Responses/PaginationResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace RestaurantServer.DTOs.Responses
+{
+    public class PaginationResponse
+    {
+        public PaginationResponse(int currentPage, int pageSize, int totalRecords, int totalPages)
+        {
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalRecords = totalRecords;
+            TotalPages = totalPages;
+        }
+
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalRecords { get; set; }
+        public int TotalPages { get; set; }
+
+        public bool HasNext => CurrentPage < TotalPages;
+        public bool HasPrevious => CurrentPage > 1;
+
+        public int? NextPage => HasNext ? CurrentPage + 1 : (int?)null;
+        public int? PreviousPage => HasPrevious ? CurrentPage - 1 : (int?)null;
+    }
+}
